84bd1066cbf354c8ef5896058046abf5e7c9ce5a
Fal-hi <syaifal.illahi@gmail.com>
2022-03-09 14:36:39 +0700

    baru

--- public/js/script.js
+++ public/js/script.js
@@ -52,11 +52,31 @@ document.addEventListener("DOMContentLoaded", () => {
   responsive();
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".own4").ownCarousel({
+    itemPerRow: 4,
+    // autoplay: 5000,
+    nav: true,
+    responsive: {
+      1000: [4, 24],
+      900: [3, 32.5],
+      800: [3, 32.5],
+      700: [2, 49],
+      600: [2, 49],
+      500: [1, 100],
+      400: [1, 100],
+    },
+  });
+  responsive();
+});
+
 //Get the button
 const upButton = document.getElementById("upButton");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
