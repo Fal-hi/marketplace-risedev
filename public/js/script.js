1e934582ed354299a5a56e500368f7cb9fac9ecf
Fal-hi <syaifal.illahi@gmail.com>
2022-05-30 15:27:05 +0700

    data undangan telah diperbarui

--- public/js/script.js
+++ public/js/script.js
@@ -70,24 +70,18 @@ document.addEventListener("DOMContentLoaded", () => {
   responsive();
 });
 
-//Get the button
-const upButton = document.getElementById("upButton");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    upButton.style.display = "block";
-  } else {
-    upButton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// const upButton = document.getElementById("upButton");
+// window.onscroll = function() {
+//   scrollFunction();
+// };
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     upButton.style.display = "block";
+//   } else {
+//     upButton.style.display = "none";
+//   }
+// }
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
